b5f416684042abd01dab223903d3a706525df4f0
danxguerrero <danxguerrero@yahoo.com>
2026-01-31 15:46:32 -0500

    Design the SignInPage and SignUpPage componenets

--- src/pages/auth/SignInPage.tsx
+++ src/pages/auth/SignInPage.tsx
@@ -1,11 +1,14 @@
 import AuthForm from './AuthForm'
+import FormContainer from './FormContainer';
+import { Link } from 'react-router-dom';
 
 
 const SignInPage = () => {
     return (
-        <div className="flex items-center justify-center">
-            <AuthForm fields={[{label: "username", type: "text"}, {label: "password", type: "password"}]} submitButtonLabel="Sign In"/>
-        </div>
+            <FormContainer>
+                <AuthForm fields={[{ label: "username", type: "text" }, { label: "password", type: "password" }]} submitButtonLabel="Sign In" />
+                <Link to="/sign-up" className="text-sm text-green-600 underline">Create an account</Link>
+            </FormContainer>
     )
 }
 
--- src/pages/auth/SignUpPage.tsx
+++ src/pages/auth/SignUpPage.tsx
@@ -1,12 +1,20 @@
 import AuthForm from "./AuthForm";
-
+import FormContainer from "./FormContainer";
+import {Link} from "react-router-dom";
 
 const SignUpPage = () => {
 
     return (
-        <div className="flex justify-center items-center">
-            <AuthForm fields={[{ label: "username", type: "text" }, { label: "password", type: "password" }, { label: "confirm password", type: "password" }]} submitButtonLabel="Create Account" />
-        </div>
+        <FormContainer>
+            <AuthForm
+                fields={[
+                    { label: "username", type: "text" },
+                    { label: "password", type: "password" },
+                    { label: "confirm password", type: "password" }]} 
+                submitButtonLabel="Create Account" 
+            />
+            <Link to="/" className="text-sm text-green-600 underline">Already have an account? Sign in</Link>
+        </FormContainer>
     )
 }
 
