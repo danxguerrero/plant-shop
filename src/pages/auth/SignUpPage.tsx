9439627ae0bfdc856d28bedeb73a384c398a475a
danxguerrero <danxguerrero@yahoo.com>
2026-02-02 13:11:42 -0500

    Implement Sign-In with error handling and redirect from Sign-up when account creation is successful

--- src/pages/auth/SignUpPage.tsx
+++ src/pages/auth/SignUpPage.tsx
@@ -1,5 +1,5 @@
 import {useState} from 'react'
-import {Link} from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import AuthForm from "./AuthForm";
 import FormContainer from "./FormContainer";
 import * as userService from '@/services/user'
@@ -7,6 +7,8 @@ import * as userService from '@/services/user'
 
 const SignUpPage = () => {
     const [error,setError] = useState<string>("")
+    const navigate = useNavigate();
+
     const onSubmit = async (values: {[key:string]: string}) => {
         if (values.username.length < 4) {
             setError("Username too short");
@@ -29,6 +31,11 @@ const SignUpPage = () => {
         if (response.status == 201) {
             console.log("user created")
             setError('')
+            navigate('/', {
+                state: {
+                    accountCreated: true
+                }
+            })
         } else {
             const data = await response.json()
             console.log(data)
