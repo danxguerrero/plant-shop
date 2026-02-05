cac975f74210163b98f61802286d575ae76fd65c
danxguerrero <danxguerrero@yahoo.com>
2026-02-05 15:45:05 -0500

    Link NavBar logo to PlantListPage

--- src/shared-components/NavBar/index.tsx
+++ src/shared-components/NavBar/index.tsx
@@ -1,4 +1,5 @@
 import { useContext, useState } from "react";
+import { Link } from "react-router-dom";
 import SessionContext from "@/contexts/SessionContext";
 
 const NavBar = () => {
@@ -7,18 +8,20 @@ const NavBar = () => {
   const username = sessionContext?.username;
 
   return (
-    <nav 
+    <nav
       className="flex justify-center bg-emerald-800"
       onMouseLeave={() => setUserMenuOpen(false)}
     >
       <div className="flex w-full max-w-5xl items-center justify-between px-8 py-2">
-        <div className="flex flex-col items-center font-playfair text-2xl text-white">
-          <img
-            className="w-10"
-            src="https://static-task-assets.react-formula.com/capstone_logo_light.png"
-          />
-          Plant Shop
-        </div>
+        <Link to="/plants">
+          <div className="flex flex-col items-center font-playfair text-2xl text-white">
+            <img
+              className="w-10"
+              src="https://static-task-assets.react-formula.com/capstone_logo_light.png"
+            />
+            Plant Shop
+          </div>
+        </Link>
         <div className="flex flex-1 justify-end">
           <div className="relative min-w-32">
             <button
@@ -29,8 +32,9 @@ const NavBar = () => {
               {username}
             </button>
             {userMenuOpen && (
-              <div className="absolute bottom-[-46px] left-0 mt-20 bg-white px-4 py-2 rounded-md shadow-md">
-                <button className="text-slate-500 hover:text-emerald-700"
+              <div className="absolute bottom-[-46px] left-0 mt-20 rounded-md bg-white px-4 py-2 shadow-md">
+                <button
+                  className="text-slate-500 hover:text-emerald-700"
                   onClick={sessionContext?.signOut}
                 >
                   <i className="fa-solid fa-arrow-right-from-bracket mr-2"></i>
