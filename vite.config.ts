75513010c657ebedfc337b1e81efe046f14e920f
danxguerrero <danxguerrero@yahoo.com>
2026-02-06 15:20:36 -0500

    Update vite.config.ts to only look for tsconfig files in the current project

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigpaths from 'vite-tsconfig-paths'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigpaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigpaths()],
-})
+  plugins: [
+    react(),
+    tsconfigpaths({
+      projects: ["./tsconfig.app.json"],
+    }),
+  ],
+});
